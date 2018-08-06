b2f922a355008c69f84b92e7c66ac63b10911c27
Brittanie Pham <brittaniepham@gmail.com>
2018-08-06 15:48:05 -0600

    responsive thank you video

--- client/src/components/ThankYou.js
+++ client/src/components/ThankYou.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react'
+import { Container, Grid } from 'semantic-ui-react'
 import styled from 'styled-components'
 
 const ThankYou = () => {
   return (
-    <VideoContainer textAlign='center'>
-      <iframe
+    <RespContainer textAlign='center'>
+      <Iframe
         width="1020" height="574" src="https://www.youtube.com/embed/KTgICYeMPbg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen title='thanks'>
-      </iframe>
-    </VideoContainer>
+      </Iframe>
+    </RespContainer>
   );
 };
 
-const VideoContainer = styled(Container)`
-  display: flex;
-  justify-content: space-around;
-  flex-direction: column;
-  align-items: center;
-  padding: 50px;
+const Iframe = styled.iframe `
+position: absolute;
+top: 0;
+left: 0;
+width: 100%;
+height: 100%;
+border: 0;
+`
+
+const RespContainer = styled.div `
+  position: relative;
+  overflow: hidden;
+  padding-top: 56.25%;
 `
 
 export default ThankYou;
