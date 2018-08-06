cb7c0e56b57d9d68cc6bd78534b544aba55aac42
Brittanie Pham <brittaniepham@gmail.com>
2018-08-06 16:01:12 -0600

    remove linter warnings

--- client/src/components/ThankYou.js
+++ client/src/components/ThankYou.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Grid } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
 import styled from 'styled-components'
 
 const ThankYou = () => {
